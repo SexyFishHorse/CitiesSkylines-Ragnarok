05834f82e397de4a09e13b79c348aa1b49d02dca
Asbjørn Hansen <ah@fenris.io>
2016-12-03 14:40:50 +0100

    enabled auto-evacuate for disasters

--- Ragnarok/RagnarokUserMod.cs
+++ Ragnarok/RagnarokUserMod.cs
@@ -1,6 +1,7 @@
 ﻿namespace SexyFishHorse.CitiesSkylines.Ragnarok
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using ICities;
     using Infrastructure;
@@ -8,15 +9,28 @@
     using Infrastructure.UI;
     using JetBrains.Annotations;
     using Logger;
+    using UnityEngine;
+    using ILogger = Logger.ILogger;
 
     [UsedImplicitly]
-    public class RagnarokUserMod : UserModBase, IDisasterExtension
+    public class RagnarokUserMod : UserModBase, IDisasterExtension, ILoadingExtension
     {
         private const string ModName = "Ragnarok";
 
+        private static readonly string[] AutoEvacuateValues =
+        {
+            "Disabled",
+            "Enabled - manual release",
+            "Enabled - auto release"
+        };
+
         private readonly ILogger logger;
 
+        private readonly HashSet<ushort> manualReleaseDisasters = new HashSet<ushort>();
+
         private DisasterWrapper disasterWrapper;
+
+        private WarningPhasePanel phasePanel;
 
         static RagnarokUserMod()
         {
@@ -50,24 +64,18 @@
         {
             logger.Info("OnCreated " + disaster);
 
-            disasterWrapper = (DisasterWrapper)disaster;
+            disasterWrapper = (DisasterWrapper) disaster;
 
-            DisasterManager.instance.m_disableAutomaticFollow = !ConfigurationManager.Instance.GetSetting<bool>(SettingKeys.DisableAutofocusDisaster);
+            DisasterManager.instance.m_disableAutomaticFollow =
+                !ConfigurationManager.Instance.GetSetting<bool>(SettingKeys.DisableAutofocusDisaster);
 
-            var field = disasterWrapper.GetType()
-                                       .GetField("m_DisasterTypeToInfoConversion", BindingFlags.NonPublic | BindingFlags.Instance);
+            logger.Info("disableautomaticfollow is " + DisasterManager.instance.m_disableAutomaticFollow);
 
-            var convertionDictionary = new Dictionary<DisasterType, DisasterInfo>();
-            convertionDictionary[DisasterType.Earthquake] = DisasterManager.FindDisasterInfo<EarthquakeAI>();
-            convertionDictionary[DisasterType.ForestFire] = DisasterManager.FindDisasterInfo<ForestFireAI>();
-            convertionDictionary[DisasterType.MeteorStrike] = DisasterManager.FindDisasterInfo<MeteorStrikeAI>();
-            convertionDictionary[DisasterType.ThunderStorm] = DisasterManager.FindDisasterInfo<ThunderStormAI>();
-            convertionDictionary[DisasterType.Tornado] = DisasterManager.FindDisasterInfo<TornadoAI>();
-            convertionDictionary[DisasterType.Tsunami] = DisasterManager.FindDisasterInfo<TsunamiAI>();
-            convertionDictionary[DisasterType.StructureCollapse] = DisasterManager.FindDisasterInfo<StructureCollapseAI>();
-            convertionDictionary[DisasterType.StructureFire] = DisasterManager.FindDisasterInfo<StructureFireAI>();
-            convertionDictionary[DisasterType.Sinkhole] = DisasterManager.FindDisasterInfo<SinkholeAI>();
-            field.SetValue(disasterWrapper, convertionDictionary);
+            SetConvertionTable();
+        }
+
+        public void OnCreated(ILoading loading)
+        {
         }
 
         public void OnDisasterActivated(ushort disasterId)
@@ -77,10 +85,7 @@
         public void OnDisasterCreated(ushort disasterId)
         {
             var disasterInfo = disasterWrapper.GetDisasterSettings(disasterId);
-            logger.Info("Created disaster type {0}, name: {1}", disasterInfo.type, disasterInfo.name);
-
-            var isCustom = disasterWrapper.IsCustomDisaster(disasterId);
-            logger.Info("is custom? {0}", isCustom);
+            logger.Info("Created disaster type {0} with name {1}", disasterInfo.type, disasterInfo.name);
 
             var settingKey = GetDisabledSettingKeyForDisasterType(disasterInfo.type);
 
@@ -93,18 +98,65 @@
             if (ConfigurationManager.Instance.GetSetting<bool>(settingKey))
             {
                 logger.Info("Deactivating disaster");
-                disasterWrapper.DeactivateDisaster(disasterId);
+                disasterWrapper.EndDisaster(disasterId);
             }
         }
 
         public void OnDisasterDeactivated(ushort disasterId)
         {
-            // todo: stop evac logic
+            var disasterInfo = disasterWrapper.GetDisasterSettings(disasterId);
+
+            logger.Info("Disaster {0} with name {1} over", disasterInfo.type, disasterInfo.name);
+
+            if (disasterInfo.type == DisasterType.Empty)
+            {
+                return;
+            }
+
+            if (!IsEvacuating())
+            {
+                logger.Info("Not evacuating. Clear list of active manual release disasters");
+                manualReleaseDisasters.Clear();
+                return;
+            }
+
+            if (ShouldAutoRelease(disasterInfo.type) && !manualReleaseDisasters.Any())
+            {
+                logger.Info("Auto releasing citizens");
+                DisasterManager.instance.EvacuateAll(true);
+            }
         }
 
         public void OnDisasterDetected(ushort disasterId)
         {
-            // todo: start evac logic
+            var disasterInfo = disasterWrapper.GetDisasterSettings(disasterId);
+
+            logger.Info("Detected {0} with name {1}", disasterInfo.type, disasterInfo.name);
+
+            if (disasterInfo.type == DisasterType.Empty)
+            {
+                return;
+            }
+
+            if (ShouldAutoEvacuate(disasterInfo.type))
+            {
+                logger.Info("Is auto-evacuate disaster");
+                if (!IsEvacuating())
+                {
+                    logger.Info("Starting evacuation");
+                    DisasterManager.instance.EvacuateAll(false);
+                }
+                else
+                {
+                    logger.Info("Already evacuating");
+                }
+
+                if (ShouldManualRelease(disasterInfo.type))
+                {
+                    logger.Info("Should be manually released");
+                    manualReleaseDisasters.Add(disasterId);
+                }
+            }
         }
 
         public void OnDisasterFinished(ushort disasterId)
@@ -112,6 +164,27 @@
         }
 
         public void OnDisasterStarted(ushort disasterId)
+        {
+        }
+
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            if ((mode != LoadMode.NewGame) || (mode != LoadMode.LoadGame))
+            {
+                return;
+            }
+
+            phasePanel = Object.FindObjectOfType<WarningPhasePanel>();
+
+            BuildingManager.instance.m_firesDisabled =
+                ConfigurationManager.Instance.GetSetting<bool>(SettingKeys.DisableNonDisasterFires);
+        }
+
+        public void OnLevelUnloading()
+        {
+        }
+
+        public void OnReleased()
         {
         }
 
@@ -137,6 +210,30 @@
             AddEnabledDisasterCheckbox(enabledDisastersGroup, "Thunderstorms", SettingKeys.DisableThunderstorms);
             AddEnabledDisasterCheckbox(enabledDisastersGroup, "Tornadoes", SettingKeys.DisableTornadoes);
             AddEnabledDisasterCheckbox(enabledDisastersGroup, "Tsunamis", SettingKeys.DisableTsunamis);
+
+            enabledDisastersGroup.AddCheckBox("Non-disaster related fires",
+                                              ConfigurationManager.Instance.GetSetting<bool>(SettingKeys.DisableNonDisasterFires),
+                                              OnDisableNonDisasterFiresChanged);
+
+            var autoEvacuateGroup = uiHelper.AddGroup("Auto-evacuation behaviour");
+
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Forest fires", SettingKeys.AutoEvacuateForestFires);
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Earthquakes", SettingKeys.AutoEvacuateEarthquake);
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Meteors", SettingKeys.AutoEvacuateMeteors);
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Sinkholes", SettingKeys.AutoEvacuateSinkholes);
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Structure collapses", SettingKeys.AutoEvacuateStructureCollapses);
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Structure fires", SettingKeys.AutoEvacuateStructureFires);
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Thunderstorms", SettingKeys.AutoEvacuateThunderstorms);
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Tornadoes", SettingKeys.AutoEvacuateTornadoes);
+            AddAutoEvacuateBehaviourDropDown(autoEvacuateGroup, "Tsunamis", SettingKeys.AutoEvacuateTsunamis);
+        }
+
+        private void AddAutoEvacuateBehaviourDropDown(StronglyTypedUiHelper autoEvacuateGroup, string label,
+                                                      string settingKey)
+        {
+            autoEvacuateGroup.AddDropDown(label, AutoEvacuateValues,
+                                          ConfigurationManager.Instance.GetSetting<int>(settingKey),
+                                          sel => SaveSetting(settingKey, sel));
         }
 
         private void AddEnabledDisasterCheckbox(StronglyTypedUiHelper uiGroup, string label, string settingKey)
@@ -172,6 +269,17 @@
             }
         }
 
+        private bool IsEvacuating()
+        {
+            var field = phasePanel.GetType().GetField("m_isEvacuating", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var isEvacuating = (bool) field.GetValue(phasePanel);
+
+            logger.Info("Is evacuating: " + isEvacuating);
+
+            return isEvacuating;
+        }
+
         private void OnDisableAutoFocusDisasterChanged(bool isChecked)
         {
             SaveSetting(SettingKeys.DisableAutofocusDisaster, isChecked);
@@ -184,11 +292,61 @@
             }
         }
 
+        private void OnDisableNonDisasterFiresChanged(bool isChecked)
+        {
+            SaveSetting(SettingKeys.DisableNonDisasterFires, isChecked);
+
+            if (BuildingManager.exists)
+            {
+                BuildingManager.instance.m_firesDisabled = isChecked;
+            }
+        }
+
         private void SaveSetting(string settingKey, object value)
         {
             logger.Info("Saving setting {0} with value {1}", settingKey, value);
 
             ConfigurationManager.Instance.SaveSetting(settingKey, value);
+        }
+
+        private void SetConvertionTable()
+        {
+            var field = disasterWrapper.GetType()
+                                       .GetField("m_DisasterTypeToInfoConversion",
+                                                 BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var convertionDictionary = new Dictionary<DisasterType, DisasterInfo>();
+            convertionDictionary[DisasterType.Earthquake] = DisasterManager.FindDisasterInfo<EarthquakeAI>();
+            convertionDictionary[DisasterType.ForestFire] = DisasterManager.FindDisasterInfo<ForestFireAI>();
+            convertionDictionary[DisasterType.MeteorStrike] = DisasterManager.FindDisasterInfo<MeteorStrikeAI>();
+            convertionDictionary[DisasterType.ThunderStorm] = DisasterManager.FindDisasterInfo<ThunderStormAI>();
+            convertionDictionary[DisasterType.Tornado] = DisasterManager.FindDisasterInfo<TornadoAI>();
+            convertionDictionary[DisasterType.Tsunami] = DisasterManager.FindDisasterInfo<TsunamiAI>();
+            convertionDictionary[DisasterType.StructureCollapse] = DisasterManager.FindDisasterInfo<StructureCollapseAI>();
+            convertionDictionary[DisasterType.StructureFire] = DisasterManager.FindDisasterInfo<StructureFireAI>();
+            convertionDictionary[DisasterType.Sinkhole] = DisasterManager.FindDisasterInfo<SinkholeAI>();
+            field.SetValue(disasterWrapper, convertionDictionary);
+        }
+
+        private bool ShouldAutoEvacuate(DisasterType disasterType)
+        {
+            var settingKey = SettingKeys.AutoEvacuateSettingKeyMapping.Single(x => x.Key == disasterType).Value;
+
+            return ConfigurationManager.Instance.GetSetting<int>(settingKey) > 0;
+        }
+
+        private bool ShouldAutoRelease(DisasterType disasterType)
+        {
+            var settingKey = SettingKeys.AutoEvacuateSettingKeyMapping.Single(x => x.Key == disasterType).Value;
+
+            return ConfigurationManager.Instance.GetSetting<int>(settingKey) > 1;
+        }
+
+        private bool ShouldManualRelease(DisasterType disasterType)
+        {
+            var settingKey = SettingKeys.AutoEvacuateSettingKeyMapping.Single(x => x.Key == disasterType).Value;
+
+            return ConfigurationManager.Instance.GetSetting<int>(settingKey) == 1;
         }
     }
 }
--- Ragnarok/SettingKeys.cs
+++ Ragnarok/SettingKeys.cs
@@ -1,7 +1,28 @@
 ﻿namespace SexyFishHorse.CitiesSkylines.Ragnarok
 {
+    using System.Collections.Generic;
+    using ICities;
+
     public static class SettingKeys
     {
+        public const string AutoEvacuateEarthquake = "AutoEvacuateEarthquake";
+
+        public const string AutoEvacuateForestFires = "AutoEvacuateForestFires";
+
+        public const string AutoEvacuateMeteors = "AutoEvacuateMeteors";
+
+        public const string AutoEvacuateSinkholes = "AutoEvacuateSinkholes";
+
+        public const string AutoEvacuateStructureCollapses = "AutoEvacuateStructureCollapses";
+
+        public const string AutoEvacuateStructureFires = "AutoEvacuateStructureFires";
+
+        public const string AutoEvacuateThunderstorms = "AutoEvacuateThunderstorms";
+
+        public const string AutoEvacuateTornadoes = "AutoEvacuateTornadoes";
+
+        public const string AutoEvacuateTsunamis = "AutoEvacuateTsunamis";
+
         public const string DisableAutofocusDisaster = "DisableAutofocusDisaster";
 
         public const string DisableEarthquakes = "DisableEarthquakes";
@@ -9,6 +30,8 @@
         public const string DisableForestFires = "DisableForestFires";
 
         public const string DisableMeteors = "DisableMeteors";
+
+        public const string DisableNonDisasterFires = "DisableNonDisasterFires";
 
         public const string DisableSinkholes = "DisableSinkholes";
 
@@ -21,5 +44,33 @@
         public const string DisableTornadoes = "DisableTornadoes";
 
         public const string DisableTsunamis = "DisableTsunamis";
+
+        public static readonly IDictionary<DisasterType, string> AutoEvacuateSettingKeyMapping = new Dictionary
+            <DisasterType, string>
+            {
+                {DisasterType.Earthquake, AutoEvacuateEarthquake},
+                {DisasterType.ForestFire, AutoEvacuateForestFires},
+                {DisasterType.MeteorStrike, AutoEvacuateMeteors},
+                {DisasterType.Sinkhole, AutoEvacuateSinkholes},
+                {DisasterType.StructureCollapse, AutoEvacuateStructureCollapses},
+                {DisasterType.StructureFire, AutoEvacuateStructureFires},
+                {DisasterType.ThunderStorm, AutoEvacuateThunderstorms},
+                {DisasterType.Tornado, AutoEvacuateTornadoes},
+                {DisasterType.Tsunami, AutoEvacuateTsunamis}
+            };
+
+        public static readonly IDictionary<DisasterType, string> DisableDisasterSettingKeyMapping = new Dictionary
+            <DisasterType, string>
+            {
+                {DisasterType.Earthquake, DisableEarthquakes},
+                {DisasterType.ForestFire, DisableForestFires},
+                {DisasterType.MeteorStrike, DisableMeteors},
+                {DisasterType.Sinkhole, DisableSinkholes},
+                {DisasterType.StructureCollapse, DisableStructureCollapses},
+                {DisasterType.StructureFire, DisableStructureFires},
+                {DisasterType.ThunderStorm, DisableThunderstorms},
+                {DisasterType.Tornado, DisableTornadoes},
+                {DisasterType.Tsunami, DisableTsunamis}
+            };
     }
 }
