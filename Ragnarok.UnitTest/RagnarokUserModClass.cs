352d942d2f9203ce71e8f7be3fde7d390b879519
Asbjørn Hansen <ah@fenris.io>
2016-12-20 21:24:47 +0100

    added unit tests for private fields check

---
+++ Ragnarok.UnitTest/RagnarokUserModClass.cs
@@ -0,0 +1,30 @@
+﻿namespace SexyFishHorse.CitiesSkylines.Ragnarok.UnitTest
+{
+    using System.Reflection;
+    using FluentAssertions;
+    using Xunit;
+
+    public class RagnarokUserModClass
+    {
+        public class PrivateFieldsCheck
+        {
+            [Fact]
+            public void WarningPhasePanelShouldHavePrivateFieldForIsEvacuating()
+            {
+                var evacuatingField = typeof(WarningPhasePanel).GetField("m_isEvacuating", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                evacuatingField.Should().NotBeNull();
+            }
+
+            [Fact]
+            public void DisasterWrapperShouldHavePrivateFieldForDisasterTypeInfoConvertion()
+            {
+                var convertionField = typeof(DisasterWrapper).GetField(
+                    "m_DisasterTypeToInfoConversion",
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+
+                convertionField.Should().NotBeNull();
+            }
+        }
+    }
+}
