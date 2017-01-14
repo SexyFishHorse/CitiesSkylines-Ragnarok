270adbb27bf79aad2f635e87427653c2cf3cc624
Asbjørn Hansen <ah@fenris.io>
2017-01-14 01:56:54 +0100

    added all disaster names to enum class

--- Ragnarok/Model/DisasterInfoNames.cs
+++ Ragnarok/Model/DisasterInfoNames.cs
@@ -2,6 +2,26 @@
 {
     public static class DisasterInfoNames
     {
+        public const string Chirpynado = "Chirpynado";
+
+        public const string Earthquake = "Earthquake";
+
+        public const string ForestFire = "Forest Fire";
+
         public const string GenericFlood = "Generic Flood";
+
+        public const string MeteorStrike = "Meteor Strike";
+
+        public const string Sinkhole = "Sinkhole";
+
+        public const string StructureCollapse = "Structure Collapse";
+
+        public const string StructureFire = "Structure Fire";
+
+        public const string Thunderstorm = "Thunderstorm";
+
+        public const string Tornado = "Tornado";
+
+        public const string Tsunami = "Tsunami";
     }
 }
